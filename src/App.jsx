7dc873a40f27371b3160affb69edfbf51838cc66
Fahim-H <fahimlutful@gmail.com>
2022-09-13 14:16:47 +0600

    add some form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Form from './pages/Form';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import AddDoctor from './pages/AddDoctor';
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Form getLoginDetail />} />
+        <Route path="/" element={<Form />} />
 
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/AddDoctor" element={<AddDoctor />} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,5 @@ module.exports = {
   daisyui: {
     themes: false,
   },
-  plugins: [require('daisyui')],
+  plugins: [require('daisyui'), require('flowbite/plugin')],
 };
